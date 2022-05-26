08686f0776ddc87578935d7219157cfe27961375
Daffa Nabil Libriana <ndaffanabil@gmail.com>
2022-05-26 09:46:03 +0000

    adding Bismillah

--- CC/main.py
+++ CC/main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "Word")
+    name = os.environ.get("NAME", "Bismillah")
     return "Hello {}!".format(name)
 
 
