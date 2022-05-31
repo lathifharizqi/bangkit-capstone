ecb9284f2e19ae64825d2c42c8fa5f2cd4f2bece
Daffa Nabil Libriana <ndaffanabil@gmail.com>
2022-05-31 10:59:01 +0000

    revission login api and adding comment api

--- CC/main.py
+++ CC/main.py
@@ -15,36 +15,45 @@ def api():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    paramUsername = str(request.args.get("username"))
-    paramPass = str(request.args.get("password"))
 
-    #database connect
-    cnx = mysql.connector.connect(user='root', password='123', host='34.68.201.197', database='femeow')
-    cursor = cnx.cursor()
+    if request.method == 'POST':
 
-    #query
-    cursor.execute("select * from user;")
-    row_headers=[x[0] for x in cursor.description]
-    rv = cursor.fetchall()
-    json_data = []
-    for result in rv:
-        json_data.append(dict(zip(row_headers,result)))
-    cnx.close()
+        json = request.json
 
-    #finding algorithm
-    result = next((item for item in json_data if item["username"] == paramUsername and item["password"] == paramPass), None)
-    if result != None:
-        jsonResult = {
-            "error" : False,
-            "message" : "success",
-            "loginResult" : result
-        }
+       #database connect
+        cnx = mysql.connector.connect(user='root', password='123', host='34.68.201.197', database='femeow')
+        cursor = cnx.cursor()
+
+        #query
+        cursor.execute("select * from user;")
+        row_headers=[x[0] for x in cursor.description]
+        rv = cursor.fetchall()
+        json_data = []
+        for result in rv:
+            json_data.append(dict(zip(row_headers,result)))
+        cnx.close()
+
+        #finding algorithm
+        result = next((item for item in json_data if item["username"] == json['username'] and item["password"] == json['password']), None)
+        if result != None:
+            jsonResult = {
+                "error" : False,
+                "message" : "success",
+                "loginResult" : result
+            }
+        else:
+            jsonResult = {
+                "error" : True,
+                "message" : "failed",
+            }
+        return jsonify(jsonResult)
+
     else:
         jsonResult = {
-            "error" : True,
-            "message" : "failed",
-        }
-    return jsonify(jsonResult)
+                "error" : True,
+                "message" : "failed",
+            }
+        return (jsonResult)
 
 @app.route("/forum", methods=['GET', 'POST'])
 def forum():
@@ -73,7 +82,7 @@ def forum():
             return 'Content-Type not supported!'
     else:
        #query
-        cursor.execute("select * from forum;")
+        cursor.execute("SELECT f.idPost, f.title, f.body, f.haveImage, f.imageBase64, f.breed, f.dateCreated, f.createdBy, u.nama_lengkap FROM forum f LEFT OUTER JOIN user u ON f.createdBy=u.username;")
         row_headers=[x[0] for x in cursor.description]
         rv = cursor.fetchall()
         json_data = []
@@ -90,7 +99,52 @@ def forum():
 
         return jsonify(jsonResult)
     
+@app.route("/comment", methods=['GET', 'POST'])
+def comment():
+    #database connect
+    cnx = mysql.connector.connect(user='root', password='123', host='34.68.201.197', database='femeow')
+    cursor = cnx.cursor()
 
+    if request.method == 'POST':
+        content_type = request.headers.get('Content-Type')
+        if (content_type == 'application/json'):
+            json = request.json
+
+            #query
+            query = "INSERT INTO `comment` (`idComment`, `dateCreated`, `body`, `createdBy`, `idPost`) VALUES (NULL, '{}', '{}', '{}', '{}')".format(json['dateCreated'],json['body'],json['createdBy'],json['idPost'])
+            cursor.execute(query)
+            result = cursor.fetchone()
+            cnx.commit()
+            cnx.close()
+
+            result = {
+                "error" : False,
+                "message" : "success",
+            }
+            return result
+        else:
+            return 'Content-Type not supported!'
+    else:
+
+        paramIdPost = str(request.args.get("idPost"))
+       #query
+        query="SELECT c.idPost, c.body, c.dateCreated, c.createdBy, u.nama_lengkap FROM comment c LEFT OUTER JOIN user u ON c.createdBy=u.username WHERE c.idPost = {}".format(paramIdPost)
+        cursor.execute(query)
+        row_headers=[x[0] for x in cursor.description]
+        rv = cursor.fetchall()
+        json_data = []
+        for result in rv:
+            json_data.append(dict(zip(row_headers,result)))
+        cnx.close()
+
+        jsonResult = {
+                "error" : False,
+                "message" : "success",
+                "getCommentResult" : json_data
+            }
+
+
+        return jsonify(jsonResult)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
